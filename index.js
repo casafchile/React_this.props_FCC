17d89f3b5f313a26c8927c0292eb3c6a9675f296
Felipe Herrera <83690907+casafchile@users.noreply.github.com>
2024-07-01 20:40:06 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,31 @@
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+  }
+  render() {
+    return (
+        <div>
+            { /* Change code below this line */ }
+            <Welcome name="ola"/>                            //se ingresa el dato
+            { /* Change code above this line */ }
+        </div>
+    );
+  }
+};
+
+class Welcome extends React.Component {
+  constructor(props) {
+    super(props);
+
+  }
+  render() {
+    return (
+        <div>
+          { /* Change code below this line */ }
+          <p>Hello, <strong>{this.props.name}</strong>!</p>     //muestra el dato
+          { /* Change code above this line */ }
+        </div>
+    );
+  }
+};
